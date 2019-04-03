ae1dd707ac755cae2f02d649c0176e07a03eeff9
Will Fergus <will.fergus@loop.colum.edu>
2019-04-03 18:19:01 -0500

    adds Astar ai scripts

---
+++ Assets/Scripts/AI and FSM/Node.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node : IHeapItem<Node>
+{
+    public bool IsWalkable;
+    public Vector3 WorldPosition;
+    public int GridX;
+    public int GridY;
+
+    public int gCost;
+    public int hCost;
+    public Node parent;
+   
+
+    public Node(bool isWalkable, Vector3 worldPosition, int gridX, int gridY)
+    {
+        IsWalkable = isWalkable;
+        WorldPosition = worldPosition;
+        GridX = gridX;
+        GridY = gridY;
+    }
+
+    public int fCost
+    {
+        get { return gCost + hCost; }
+    }
+
+    public int HeapIndex
+    {
+        get
+        {
+            return HeapIndex;
+        }
+        set
+        {
+            HeapIndex = value;
+        }
+    }
+
+    public int CompareTo(Node nodeToCompare)
+    {
+        int compare = fCost.CompareTo(nodeToCompare.fCost);
+        if (compare == 0)
+        {
+            compare = hCost.CompareTo(nodeToCompare.hCost);
+        }
+        return -compare;
+    }
+}
---
+++ Assets/Scripts/AI and FSM/Pathfinding.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Pathfinding : MonoBehaviour
+{
+    public Transform seeker, target;
+
+    Grid grid;
+    private void Update()
+    {
+        FindPath(seeker.position, target.position);
+    }
+    void Awake()
+    {
+        grid = GetComponent<Grid>();
+    }
+    void FindPath(Vector3 startPos, Vector3 targetPos)
+    {
+        Node startNode = grid.NodeFromWorldPoint(startPos);
+        Node targetNode = grid.NodeFromWorldPoint(targetPos);
+
+        List<Node> openSet = new List<Node>();
+        HashSet<Node> closedSet = new HashSet<Node>();
+
+        openSet.Add(startNode);
+
+        while (openSet.Count > 0)
+        {
+            Node currentNode = openSet[0];
+            for (int i = 1; i < openSet.Count; i++)
+            {
+                if (openSet[i].fCost < currentNode.fCost || openSet[i].fCost == currentNode.fCost && openSet[i].hCost < currentNode.hCost)
+                {
+                    currentNode = openSet[i];
+                }
+            }
+
+            openSet.Remove(currentNode);
+            closedSet.Add(currentNode);
+
+            if (currentNode == targetNode)
+            {
+                RetracePath(startNode, targetNode);
+                return;
+            }
+
+            foreach (Node neighbor in grid.GetNeighbors(currentNode))
+            {
+                if (!neighbor.IsWalkable || closedSet.Contains(neighbor))
+                {
+                    continue;
+                }
+                    int newMovementCostToNeighbor = currentNode.gCost + GetDistance(currentNode, neighbor);
+                if (newMovementCostToNeighbor < neighbor.gCost || !openSet.Contains(neighbor))
+                {
+                    neighbor.gCost = newMovementCostToNeighbor;
+                    neighbor.hCost = GetDistance(neighbor, targetNode);
+                    neighbor.parent = currentNode;
+
+                    if (!openSet.Contains(neighbor))
+                        openSet.Add(neighbor);
+                }
+            }
+        }
+
+        void RetracePath(Node startingNode, Node endingNode)
+        {
+            List<Node> path = new List<Node>();
+            Node currentNode = endingNode;
+
+            while (currentNode != startingNode)
+            {
+                path.Add(currentNode);
+                currentNode = currentNode.parent;
+            }
+            path.Reverse();
+
+            grid.path = path;
+        }
+
+        int GetDistance(Node nodeA, Node nodeB)
+        {
+            int distX = Mathf.Abs(nodeA.GridX - nodeB.GridX);
+            int distY = Mathf.Abs(nodeA.GridY - nodeB.GridY);
+
+            if (distX > distY)
+                return 14 * distY + 10 * (distX - distY);
+            return 14 * distX + 10 * (distY - distX);
+        }
+        
+    }
+}
