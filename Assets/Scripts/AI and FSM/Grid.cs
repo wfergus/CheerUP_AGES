ee3668adea7db99165af5ea3f951a7003536c6de
Will Fergus <will.fergus@loop.colum.edu>
2019-04-04 16:48:00 -0500

    fix

--- Assets/Scripts/AI and FSM/Grid.cs
+++ Assets/Scripts/AI and FSM/Grid.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Grid : MonoBehaviour
 {
+    public bool onlyDisplayPathGizmos;
     Node[,] grid;
     public Vector2 gridWorldSize;
     public float nodeRadius;
@@ -80,21 +81,35 @@ public class Grid : MonoBehaviour
     }
 
     public List<Node> path;
-    private void OnDrawGizmos()
+    void OnDrawGizmos()
     {
         Gizmos.DrawWireCube(transform.position, new Vector3(gridWorldSize.x, 1, gridWorldSize.y));
 
-        if (grid != null)
+        if (onlyDisplayPathGizmos)
         {
-            foreach (Node n in grid)
+            if (path != null)
             {
-                Gizmos.color = (n.IsWalkable) ? Color.white : Color.red;
-                if (path != null)
-                    if (path.Contains(n))
-                        Gizmos.color = Color.black;
-                Gizmos.DrawCube(n.WorldPosition, Vector3.one * (nodeDiameter - .1f));
+                foreach (Node n in path)
+                {
+                    Gizmos.color = Color.black;
+                    Gizmos.DrawCube(n.WorldPosition, Vector3.one * (nodeDiameter - .1f));
+                }
             }
+        }
+        else
+        {
 
+            if (grid != null)
+            {
+                foreach (Node n in grid)
+                {
+                    Gizmos.color = (n.IsWalkable) ? Color.white : Color.red;
+                    if (path != null)
+                        if (path.Contains(n))
+                            Gizmos.color = Color.black;
+                    Gizmos.DrawCube(n.WorldPosition, Vector3.one * (nodeDiameter - .1f));
+                }
+            }
         }
     }
 }
